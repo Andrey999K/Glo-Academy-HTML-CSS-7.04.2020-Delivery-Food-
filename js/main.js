5fe2970f8dacbf6def03f8b2f9b3643eb56bf580
Andrey999K <andrey1234xxxx@mail.ru>
2020-05-05 13:19:13 +0300

    Delivery food day1 part 1

--- js/main.js
+++ js/main.js
@@ -20,3 +20,19 @@ function toggleModal() {
 new WOW({
     mobile: false
 }).init();
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// НАЧАЛО ИНТЕНСИВА
+
+
+// const cartButton = document.querySelector("#cart-button");
+// const modal = document.querySelector(".modal");
+// const close = document.querySelector(".close");
+
+// cartButton.addEventListener("click", toggleModal);
+// close.addEventListener("click", toggleModal);
+
+// function toggleModal() {
+//     modal.classList.toggle("is-open");
+// }
